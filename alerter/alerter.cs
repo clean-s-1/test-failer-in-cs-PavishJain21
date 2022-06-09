b133d2e12a1fc51269c0b514001dbe4edc830213
z004ev9h <z004ev9h@MD3I69QC>
2022-06-09 11:47:07 +0530

    fixed test case issue

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,31 +1,67 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace AlerterSpace {
     class Alerter {
+
+        /// <summary>
+        /// The alert failure count
+        /// </summary>
         static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
-            // Return 200 for ok
-            // Return 500 for not-ok
-            // stub always succeeds and returns 200
-            return 200;
-        }
-        static void alertInCelcius(float farenheit) {
-            float celcius = (farenheit - 32) * 5 / 9;
-            int returnCode = networkAlertStub(celcius);
-            if (returnCode != 200) {
-                // non-ok response is not an error! Issues happen in life!
-                // let us keep a count of failures to report
-                // However, this code doesn't count failures!
-                // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
-            }
-        }
-        static void Main(string[] args) {
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
             alertInCelcius(400.5f);
             alertInCelcius(303.6f);
+            alertInCelcius(87.5f);
+            var status1 = networkAlertStub(45);
+            var status2 = networkAlertStub(85);
+            Debug.Assert(alertFailureCount == 2);
+            Debug.Assert(status1 == 200);
+            Debug.Assert(status2 == 500);
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Console.WriteLine("All is well (maybe!)\n");
+        }
+
+        /// <summary>
+        /// Networks the alert stub.
+        /// </summary>
+        /// <param name="celcius">The celcius.</param>
+        /// <returns></returns>
+        static int networkAlertStub(float celcius) {
+            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+            if (celcius < 41 && celcius < 65)
+            {
+                return 200;
+            }
+            return 500;
+        }
+
+        /// <summary>
+        /// Alerts the in celcius.
+        /// </summary>
+        /// <param name="farenheit">The farenheit.</param>
+        static void alertInCelcius(float farenheit) {
+            float celcius = ConvertFarenheitToCelcius(farenheit);
+            int returnCode = networkAlertStub(celcius);
+            if (returnCode != 200) {
+                alertFailureCount += 1;
+            }
+        }
+
+     
+        /// <summary>
+        /// Converts the farenheit to celcius.
+        /// </summary>
+        /// <param name="farenheit">The farenheit.</param>
+        /// <returns></returns>
+        private static float ConvertFarenheitToCelcius(float farenheit)
+        {
+            return (farenheit - 32) * 5 / 9;
         }
     }
 }
